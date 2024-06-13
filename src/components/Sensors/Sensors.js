3d582cd053b492a385b47844041da44bf2da0f8e
Talal-Sharaa <talal.i.sharaa@gmail.com>
2024-06-13 12:51:08 +0300

    feat: Add new SVG assets for flat platform, landscape mode left, landscape mode right, and portrait camera
    The commit adds new SVG assets for the flat platform, landscape mode left, landscape mode right, and portrait camera. These assets are uploaded from SVG Repo and transformed using SVG Repo Mixer Tools. The SVG files are added to the project's assets directory.

--- src/components/Sensors/Sensors.js
+++ src/components/Sensors/Sensors.js
@@ -1,14 +1,26 @@
 import React, {useState, useEffect} from 'react';
 import {View, Text, StyleSheet} from 'react-native';
 import GetLocation from 'react-native-get-location';
-import {gyroscope} from 'react-native-sensors';
+import {
+  accelerometer,
+  setUpdateIntervalForType,
+  SensorTypes,
+} from 'react-native-sensors';
 import {throttle} from 'lodash';
 import {useFocusEffect} from '@react-navigation/native';
-
+import ManWalking from '../../assets/man-walking.svg';
+import ManSitting from '../../assets/man-sitting.svg';
+import CarSide from '../../assets/car-side.svg';
+import FlatPlatform from '../../assets/flat-platform.svg';
+import Portrait from '../../assets/camera-portrait.svg';
+import LandscapeLeft from '../../assets/photography-landscape-mode-left.svg';
+import LandscapeRight from '../../assets/photography-landscape-mode-right.svg';
+import UpsideDown from '../../assets/inverted-yoga-posture.svg';
 const Sensors = () => {
   const [location, setLocation] = useState(null);
   const [error, setError] = useState(null);
-  const [gyroscopeData, setGyroscopeData] = useState(null);
+  const [accelerometerData, setAccelerometerData] = useState(null);
+  const [orientation, setOrientation] = useState('Unknown');
 
   useFocusEffect(
     React.useCallback(() => {
@@ -31,35 +43,51 @@ const Sensors = () => {
 
       fetchLocation();
 
-      const intervalId = setInterval(fetchLocation, 10000);
+      const intervalId = setInterval(fetchLocation, 6000);
 
       const throttledLog = throttle(({x, y, z, timestamp}) => {
-        // Convert radian to degree
-        const xDegree = x * (180 / Math.PI);
-        const yDegree = y * (180 / Math.PI);
-        const zDegree = z * (180 / Math.PI);
+        setAccelerometerData({x, y, z, timestamp});
 
-        setGyroscopeData({xDegree, yDegree, zDegree, timestamp});
+        // Determine orientation
+        const threshold = 7; // Adjust this threshold based on your needs
+        if (y > threshold) {
+          setOrientation('Portrait');
+        } else if (y < -1 * threshold) {
+          setOrientation('Upside Down');
+        } else if (x > threshold) {
+          setOrientation('Left Landscape');
+        } else if (x < -1 * threshold) {
+          setOrientation('Right Landscape');
+        } else {
+          setOrientation('Flat');
+        }
+
         console.log(
-          Number(xDegree).toFixed(5),
-          Number(yDegree).toFixed(5),
-          Number(zDegree).toFixed(5),
+          Number(x).toFixed(5),
+          Number(y).toFixed(5),
+          Number(z).toFixed(5),
         );
       }, 500);
 
-      const subscription = gyroscope.subscribe(throttledLog);
+      setUpdateIntervalForType(SensorTypes.accelerometer, 500); // Update interval to 500ms
+
+      const subscription = accelerometer.subscribe(throttledLog);
 
       return () => {
         clearInterval(intervalId);
         subscription.unsubscribe();
         setLocation(null);
-        setGyroscopeData(null);
+        setAccelerometerData(null);
         setError(null);
       };
-    }, [])
+    }, []),
   );
 
   const {latitude, longitude, altitude, speed} = location || {};
+  let displayText = '0 km/h';
+  const speedKmh = (speed * 3.6).toFixed(2);
+  displayText = `${speedKmh} km/h`;
+
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Sensors Component</Text>
@@ -69,15 +97,58 @@ const Sensors = () => {
       <Text style={styles.text}>
         Altitude: {location ? altitude : 'Fetching...'}
       </Text>
-      <Text style={styles.text}>Speed: {location ? speed : 'Fetching...'}</Text>
       <Text style={styles.text}>
-        Orientation:{' '}
-        {gyroscopeData
-          ? `${Number(gyroscopeData.xDegree).toFixed(5)}, ${Number(
-              gyroscopeData.yDegree,
-            ).toFixed(5)}, ${Number(gyroscopeData.zDegree).toFixed(5)}`
+        Speed: {location ? displayText : 'Fetching...'}
+      </Text>
+      {speedKmh >= 0 && speedKmh <= 1 && (
+        <View style={styles.iconContainer}>
+          <ManSitting width={50} height={50} style={styles.icon} />
+        </View>
+      )}
+      {speedKmh > 1 && speedKmh < 20 && (
+        <View style={styles.iconContainer}>
+          <ManWalking width={50} height={50} style={styles.icon} />
+        </View>
+      )}
+      {speedKmh >= 20 && (
+        <View style={styles.iconContainer}>
+          <CarSide width={50} height={50} style={styles.icon} />
+        </View>
+      )}
+      <Text style={styles.text}>
+        Accelerometer Data:{' '}
+        {accelerometerData
+          ? `${Number(accelerometerData.x).toFixed(5)}, ${Number(
+              accelerometerData.y,
+            ).toFixed(5)}, ${Number(accelerometerData.z).toFixed(5)}`
           : 'Fetching...'}
       </Text>
+      <Text style={styles.text}>Orientation: {orientation}</Text>
+      {orientation == 'Portrait' && (
+        <View style={styles.iconContainer}>
+          <Portrait width={50} height={50} style={styles.icon} />
+        </View>
+      )}
+      {orientation == 'Upside Down' && (
+        <View style={styles.iconContainer}>
+          <UpsideDown width={50} height={50} style={styles.icon} />
+        </View>
+      )}
+      {orientation == 'Left Landscape' && (
+        <View style={styles.iconContainer}>
+          <LandscapeLeft width={50} height={50} style={styles.icon} />
+        </View>
+      )}
+      {orientation == 'Right Landscape' && (
+        <View style={styles.iconContainer}>
+          <LandscapeRight width={50} height={50} style={styles.icon} />
+        </View>
+      )}
+      {orientation == 'Flat' && (
+        <View style={styles.iconContainer}>
+          <FlatPlatform width={50} height={50} style={styles.icon} />
+        </View>
+      )}
       {error && <Text style={styles.errorText}>Error: {error}</Text>}
     </View>
   );
@@ -94,8 +165,22 @@ const styles = StyleSheet.create({
   },
   text: {
     color: 'red',
+    fontSize: 18,
+    margin: 5,
   },
   errorText: {
     color: 'yellow',
+    fontSize: 16,
+    marginTop: 10,
+  },
+  iconContainer: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 10,
+  },
+  icon: {
+    width: 50,
+    height: 50,
+    marginTop: 10,
   },
 });
