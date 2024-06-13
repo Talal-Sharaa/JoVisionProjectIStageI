2d729a1e8b119985aec8d0981b70962b0d4aac0e
Talal-Sharaa <talal.i.sharaa@gmail.com>
2024-06-14 01:35:21 +0300

    chore: Update npm dependencies to include @react-native-menu/menu

--- src/components/Gallery/Gallery.js
+++ src/components/Gallery/Gallery.js
@@ -8,6 +8,10 @@ import {
   TouchableOpacity,
   Dimensions,
   Modal,
+  TextInput,
+  RefreshControl,
+  Platform,
+  PermissionsAndroid,
 } from 'react-native';
 import ImageView from 'react-native-image-viewing';
 import Video from 'react-native-video';
@@ -15,13 +19,44 @@ import {PhotoContext} from '../PhotoContext';
 import {useFocusEffect} from '@react-navigation/native';
 import {createThumbnail} from 'react-native-create-thumbnail';
 import PlayButton from '../../assets/play-button.svg';
+import RNFS from 'react-native-fs';
 
-const GalleryComponent = () => {
-  const {savedPhotos} = useContext(PhotoContext);
+const GalleryComponent = ({navigation}) => {
+  const {savedPhotos, setSavedPhotos} = useContext(PhotoContext);
   const [visible, setIsVisible] = useState(false);
   const [currentIndex, setCurrentIndex] = useState(0);
   const [isVideo, setIsVideo] = useState(false);
   const [thumbnails, setThumbnails] = useState({});
+  const [refreshing, setRefreshing] = useState(false);
+  const [renameModalVisible, setRenameModalVisible] = useState(false);
+  const [renameText, setRenameText] = useState('');
+  const [actionModalVisible, setActionModalVisible] = useState(false);
+  const [deleteModalVisible, setDeleteModalVisible] = useState(false);
+
+  useEffect(() => {
+    const requestStoragePermission = async () => {
+      try {
+        const granted = await PermissionsAndroid.request(
+          PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE,
+          {
+            title: 'Storage Permission',
+            buttonNeutral: 'Ask Me Later',
+            buttonNegative: 'Cancel',
+            buttonPositive: 'OK',
+          },
+        );
+        if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+          console.log('You can use the storage');
+        } else {
+          console.log('storage permission denied');
+        }
+      } catch (err) {
+        console.warn(err);
+      }
+    };
+
+    requestStoragePermission();
+  }, []);
 
   useEffect(() => {
     const loadThumbnails = async () => {
@@ -57,16 +92,53 @@ const GalleryComponent = () => {
     }, []),
   );
 
+  const handleRename = () => {
+    const updatedPhotos = [...savedPhotos];
+    const index = currentIndex;
+    const oldPath = savedPhotos[index];
+    const newPath = oldPath.replace(/[^/]*$/, renameText);
+
+    // Rename the file on the device using react-native-fs
+    RNFS.moveFile(oldPath, newPath)
+      .then(() => {
+        updatedPhotos[index] = newPath;
+        setSavedPhotos(updatedPhotos);
+      })
+      .catch(error => {
+        console.error('Failed to rename file', error);
+      });
+
+    setRenameModalVisible(false);
+    setActionModalVisible(false);
+  };
+
+  const handleDelete = () => {
+    const updatedPhotos = savedPhotos.filter(
+      (_, index) => index !== currentIndex,
+    );
+    setSavedPhotos(updatedPhotos);
+    setIsVisible(false);
+    setDeleteModalVisible(false);
+    setActionModalVisible(false);
+  };
+
+  const handlePressMedia = (item, index) => {
+    setCurrentIndex(index);
+    setActionModalVisible(true);
+  };
+
+  const onRefresh = () => {
+    setRefreshing(true);
+    // Reload the savedPhotos, if necessary
+    setRefreshing(false);
+  };
+
   const renderMedia = (item, index) => {
     if (item.endsWith('.mov')) {
       return (
         <TouchableOpacity
           key={item}
-          onPress={() => {
-            setCurrentIndex(index);
-            setIsVideo(true);
-            setIsVisible(true);
-          }}>
+          onPress={() => handlePressMedia(item, index)}>
           <View
             style={[
               styles.image,
@@ -97,13 +169,7 @@ const GalleryComponent = () => {
       return (
         <TouchableOpacity
           key={item}
-          onPress={() => {
-            setCurrentIndex(
-              images.findIndex(img => img.uri === `file://${item}`),
-            );
-            setIsVideo(false);
-            setIsVisible(true);
-          }}>
+          onPress={() => handlePressMedia(item, index)}>
           <Image
             source={{uri: `file://${item}`}}
             style={[styles.image, {width: imageSize, height: imageSize}]}
@@ -122,6 +188,9 @@ const GalleryComponent = () => {
         renderItem={({item, index}) => renderMedia(item, index)}
         numColumns={numColumns}
         columnWrapperStyle={styles.columnWrapper}
+        refreshControl={
+          <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+        }
       />
       <ImageView
         images={images}
@@ -144,51 +213,170 @@ const GalleryComponent = () => {
           </View>
         </Modal>
       )}
+      <Modal
+        visible={renameModalVisible}
+        transparent={true}
+        animationType="slide"
+        onRequestClose={() => setRenameModalVisible(false)}>
+        <View style={styles.modalContainer}>
+          <View style={styles.modalContent}>
+            <Text style={styles.modalTitle}>Rename</Text>
+            <TextInput
+              style={styles.textInput}
+              placeholder="Enter new name"
+              value={renameText}
+              onChangeText={setRenameText}
+            />
+            <View style={styles.modalButtons}>
+              <TouchableOpacity
+                onPress={() => setRenameModalVisible(false)}
+                style={styles.modalButton}>
+                <Text style={styles.modalButtonText}>Cancel</Text>
+              </TouchableOpacity>
+              <TouchableOpacity
+                onPress={handleRename}
+                style={styles.modalButton}>
+                <Text style={styles.modalButtonText}>Save</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </View>
+      </Modal>
+      <Modal
+        visible={actionModalVisible}
+        transparent={true}
+        animationType="slide"
+        onRequestClose={() => setActionModalVisible(false)}>
+        <View style={styles.modalContainer}>
+          <View style={styles.modalContent}>
+            <Text style={styles.modalTitle}>Select Action</Text>
+            <TouchableOpacity
+              onPress={() => {
+                setRenameText('');
+                setRenameModalVisible(true);
+              }}
+              style={styles.modalButton}>
+              <Text style={styles.modalButtonText}>Rename</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              onPress={() => setDeleteModalVisible(true)}
+              style={styles.modalButton}>
+              <Text style={styles.modalButtonText}>Delete</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              onPress={() => {
+                setActionModalVisible(false);
+              }}
+              style={styles.modalButton}>
+              <Text style={styles.modalButtonText}>Cancel</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </Modal>
+      <Modal
+        visible={deleteModalVisible}
+        transparent={true}
+        animationType="slide"
+        onRequestClose={() => setDeleteModalVisible(false)}>
+        <View style={styles.modalContainer}>
+          <View style={styles.modalContent}>
+            <Text style={styles.modalTitle}>Confirm Delete</Text>
+            <Text style={styles.modalText}>
+              Are you sure you want to delete this file?
+            </Text>
+            <View style={styles.modalButtons}>
+              <TouchableOpacity
+                onPress={() => setDeleteModalVisible(false)}
+                style={styles.modalButton}>
+                <Text style={styles.modalButtonText}>Cancel</Text>
+              </TouchableOpacity>
+              <TouchableOpacity
+                onPress={handleDelete}
+                style={styles.modalButton}>
+                <Text style={styles.modalButtonText}>Delete</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </View>
+      </Modal>
     </View>
   );
 };
 
-export default GalleryComponent;
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'white',
     padding: 10,
+    backgroundColor: '#fff',
   },
   title: {
     fontSize: 24,
     fontWeight: 'bold',
-    textAlign: 'center',
-    marginVertical: 10,
+    marginBottom: 10,
   },
   columnWrapper: {
     justifyContent: 'space-between',
   },
   image: {
     margin: 5,
-    resizeMode: 'cover',
-  },
-  videoPlaceholder: {
-    fontSize: 16,
-    color: 'white',
-    backgroundColor: 'black',
-    padding: 10,
     borderRadius: 5,
+  },
+  playButtonContainer: {
+    position: 'absolute',
   },
   videoContainer: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: 'rgba(0,0,0,0.9)',
+    backgroundColor: 'rgba(0, 0, 0, 0.8)',
   },
   video: {
     width: '100%',
     height: '100%',
   },
-  playButtonContainer: {
-    position: 'absolute',
+  modalContainer: {
+    flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+  },
+  modalContent: {
+    width: '80%',
+    backgroundColor: '#fff',
+    padding: 20,
+    borderRadius: 10,
+  },
+  modalTitle: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 5,
+    padding: 10,
+    marginBottom: 20,
+  },
+  modalButtons: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  modalButton: {
+    padding: 10,
+  },
+  modalButtonText: {
+    fontSize: 18,
+    color: '#007BFF',
+  },
+  modalText: {
+    fontSize: 16,
+    marginBottom: 20,
+  },
+  videoPlaceholder: {
+    color: '#fff',
+    textAlign: 'center',
   },
 });
+
+export default GalleryComponent;
